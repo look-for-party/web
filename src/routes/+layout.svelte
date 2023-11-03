81748388bc02411fe4ac2493b492d93c2437bd51
chezzijr <nhuthuan0501@gmail.com>
2023-11-03 21:24:28 +0700

    feat: add tailwind and skeletonui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,2 +1,8 @@
-<nav>I am nav</nav>
-<slot></slot>
+<script>
+	import Layout from '$components/layout.svelte';
+</script>
+
+<Layout>
+  <nav class="text-secondary-500">I am nav</nav>
+  <slot />
+</Layout>
