a3eb3ab7a69a21b8880923ec04fd02230f06b3e8
felixng <ngthinh302@gmail.com>
2023-11-06 14:04:30 -0500

    style: Apply pre-commit hook changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,21 +10,18 @@ const config = {
 	content: [
 		'./src/**/*.{html,js,svelte,ts}',
 		// 3. Append the path to the Skeleton package
-		join(require.resolve(
-			'@skeletonlabs/skeleton'),
-			'../**/*.{html,js,svelte,ts}'
-		)
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
 	],
 	theme: {
-		extend: {},
+		extend: {}
 	},
 	plugins: [
 		// 4. Append the Skeleton plugin (after other plugins)
 		skeleton({
-      themes: {
-        preset: ["skeleton"]
-      }
-    })
+			themes: {
+				preset: ['skeleton']
+			}
+		})
 	]
 } satisfies Config;
 
