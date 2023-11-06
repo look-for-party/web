b0aaf18587272478fcb2b95fe5f634ef561c22e9
Harrelix <phunglegend.2004@gmail.com>
2023-11-06 10:08:35 -0500

    feat: mode switch with OS preference

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,14 @@
 <script>
 	import Layout from '$components/layout.svelte';
+	import { LightSwitch } from '@skeletonlabs/skeleton';
+	import { autoModeWatcher } from '@skeletonlabs/skeleton';
 </script>
 
+<svelte:head
+	>{@html `<script>${autoModeWatcher.toString()} autoModeWatcher();</script>`}</svelte:head
+>
 <Layout>
-  <nav class="text-secondary-500">I am nav</nav>
-  <slot />
+	<LightSwitch />
+	<nav class="text-secondary-500">I am nav</nav>
+	<slot />
 </Layout>
