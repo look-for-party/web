b09f1e896d7c8aecac492fc1707d4ec3ab2b56a0
Hung Le <39961970+Harrelix@users.noreply.github.com>
2023-11-08 13:51:05 -0500

    feat: mode switch with OS preference (#14)
    * feat: mode switch with OS preference
    
    * style: Bring imports to one line
    
    * style: apply `npm run format` changes
    
    * fix: set OS preference properly
    
    Shouldn't use autoModeWatcher along with Lightswitch component (https://www.skeleton.dev/utilities/lightswitches). Using setModeCurrent and getModeOsPrefers instead.
    
    * refactor: Remove unnecessary setting of mode on mount.
    
    ---------
    
    Co-authored-by: felixng <ngthinh302@gmail.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script>
 	import Layout from '$components/layout.svelte';
+	import { LightSwitch } from '@skeletonlabs/skeleton';
 </script>
 
 <Layout>
+	<LightSwitch />
 	<nav class="text-secondary-500">I am nav</nav>
 	<slot />
 </Layout>
