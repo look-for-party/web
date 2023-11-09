85d2bf53bf828f59eb55f116bfb75d56351daac9
Harrelix <phunglegend.2004@gmail.com>
2023-11-09 09:56:48 -0500

    feat: Add AppShell to layout

--- src/lib/components/layout.svelte
+++ src/lib/components/layout.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
+	import { AppBar, AppShell } from '@skeletonlabs/skeleton';
 	import '$src/app.postcss';
 </script>
 
-<slot />
+<AppShell regionPage="relative" slotPageHeader="sticky top-0 z-10">
+	<svelte:fragment slot="header">
+		<AppBar>Look For Party</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
+	<svelte:fragment slot="sidebarRight">Sidebar Right</svelte:fragment>
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<!-- (pageFooter) -->
+	<!-- (footer) -->
+</AppShell>
