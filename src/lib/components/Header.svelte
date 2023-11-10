2d35f3d9496f8b90421000810159f9c1e93890e7
Harrelix <phunglegend.2004@gmail.com>
2023-11-09 22:25:21 -0500

    feat: Add buttons on header depending on logged in or not.

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-	import { AppBar, getDrawerStore, type DrawerSettings } from '@skeletonlabs/skeleton';
+	import { AppBar, getDrawerStore, type DrawerSettings, Avatar } from '@skeletonlabs/skeleton';
 	import SearchBar from './SearchBar.svelte';
+	import { loggedIn } from '../stores';
 
 	// must be invoked at the top level
 	// https://www.skeleton.dev/utilities/drawers#drawer-store
@@ -23,5 +24,30 @@
 		<strong class="text-2xl">Look For Party</strong>
 	</svelte:fragment>
 
-	<svelte:fragment slot="trail"><SearchBar />(avatar)</svelte:fragment>
+	<svelte:fragment slot="trail">
+		<SearchBar />
+		{#if $loggedIn}
+			<a href="/parties" class="btn hover:variant-soft-primary">Parties</a>
+			<a href="/chats" class="btn hover:variant-soft-primary">Chats</a>
+			<a href="/resources" class="btn hover:variant-soft-primary">Resources</a>
+
+			<div class="relative inline-block">
+				<span class="badge-icon variant-filled-warning absolute -top-0 -right-0 z-10">2</span>
+				<button class="btn-icon hover:variant-soft-primary">
+					<span class="material-symbols-outlined"> notifications </span>
+				</button>
+			</div>
+
+			<Avatar
+				width="w-12"
+				border="border-4 border-surface-300-600-token hover:!border-primary-500"
+				cursor="cursor-pointer"
+			/>
+		{:else}
+			<a href="/parties" class="btn hover:variant-soft-primary">Parties</a>
+			<a href="/resources" class="btn hover:variant-soft-primary">Resources</a>
+			<a href="/signin" class="btn hover:variant-soft-primary">Sign In</a>
+			<a href="/signup" class="btn hover:variant-soft-primary">Sign Up</a>
+		{/if}
+	</svelte:fragment>
 </AppBar>
