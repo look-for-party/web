274321518eb3657601bd53572dfe256b9c68f37b
Harrelix <phunglegend.2004@gmail.com>
2023-11-12 11:57:38 -0500

    feat: More robust conversation scrolling.
    Use flex and absolute position to limit height of conversation view.

--- src/routes/chats/+page.svelte
+++ src/routes/chats/+page.svelte
@@ -166,8 +166,8 @@
 	});
 </script>
 
-<section class="card h-full">
-	<div class="chat w-full h-full grid grid-cols-1 lg:grid-cols-[20%_1fr_20%]">
+<section class="h-full">
+	<div class="w-full h-full grid grid-cols-1 lg:grid-cols-[20%_1fr_20%]">
 		<!-- Navigation -->
 		<div class="hidden lg:grid grid-rows-[auto_1fr_auto] border-r border-surface-500/30">
 			<!-- Header -->
@@ -192,37 +192,39 @@
 			<!-- <footer class="border-t border-surface-500/30 p-4">(footer)</footer> -->
 		</div>
 		<!-- Chat -->
-		<div class="grid grid-row-[1fr_auto]">
+		<div class="flex flex-col">
 			<!-- Conversation -->
-			<section bind:this={elemChat} class="max-h-[85vh] p-4 overflow-y-auto space-y-4">
-				{#each messageFeed as bubble}
-					{#if bubble.host === true}
-						<div class="grid grid-cols-[auto_1fr] gap-2">
-							<Avatar src="https://i.pravatar.cc/?img={bubble.avatar}" width="w-12" />
-							<div class="card p-4 variant-soft rounded-tl-none space-y-2">
-								<header class="flex justify-between items-center">
-									<p class="font-bold">{bubble.name}</p>
-									<small class="opacity-50">{bubble.timestamp}</small>
-								</header>
-								<p>{bubble.message}</p>
+			<section bind:this={elemChat} class="flex-grow relative">
+				<div class="p-4 overflow-y-auto space-y-4 bottom-0 top-0 absolute">
+					{#each messageFeed as bubble}
+						{#if bubble.host === true}
+							<div class="grid grid-cols-[auto_1fr] gap-2">
+								<Avatar src="https://i.pravatar.cc/?img={bubble.avatar}" width="w-12" />
+								<div class="card p-4 variant-soft rounded-tl-none space-y-2">
+									<header class="flex justify-between items-center">
+										<p class="font-bold">{bubble.name}</p>
+										<small class="opacity-50">{bubble.timestamp}</small>
+									</header>
+									<p>{bubble.message}</p>
+								</div>
 							</div>
-						</div>
-					{:else}
-						<div class="grid grid-cols-[1fr_auto] gap-2">
-							<div class="card p-4 rounded-tr-none space-y-2 {bubble.color}">
-								<header class="flex justify-between items-center">
-									<p class="font-bold">{bubble.name}</p>
-									<small class="opacity-50">{bubble.timestamp}</small>
-								</header>
-								<p>{bubble.message}</p>
+						{:else}
+							<div class="grid grid-cols-[1fr_auto] gap-2">
+								<div class="card p-4 rounded-tr-none space-y-2 {bubble.color}">
+									<header class="flex justify-between items-center">
+										<p class="font-bold">{bubble.name}</p>
+										<small class="opacity-50">{bubble.timestamp}</small>
+									</header>
+									<p>{bubble.message}</p>
+								</div>
+								<Avatar src="https://i.pravatar.cc/?img={bubble.avatar}" width="w-12" />
 							</div>
-							<Avatar src="https://i.pravatar.cc/?img={bubble.avatar}" width="w-12" />
-						</div>
-					{/if}
-				{/each}
+						{/if}
+					{/each}
+				</div>
 			</section>
 			<!-- Prompt -->
-			<section class="border-t border-surface-500/30 p-4 max-h-min">
+			<section class="border-t border-surface-500/30 p-4">
 				<div
 					class="input-group input-group-divider grid-cols-[auto_1fr_auto] rounded-container-token"
 				>
