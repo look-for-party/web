d1ab56167da1e78dddd5c2a8617f23f484a16fb6
Harrelix <phunglegend.2004@gmail.com>
2023-11-17 17:37:26 -0500

    feat: party card styling.

--- src/routes/parties/components/List.svelte
+++ src/routes/parties/components/List.svelte
@@ -5,6 +5,8 @@
 	import Filtering from './Filter.svelte';
 	import type { Filter } from './types';
 	import { partiesMockData } from '$src/lib/utils/partiesMockData';
+	import { faker } from '@faker-js/faker/locale/af_ZA';
+	import { interests } from '$src/lib/partyRequirements/interests';
 
 	let filter: Filter = {
 		skills: [],
@@ -20,27 +22,58 @@
 	let parties = partiesMockData;
 </script>
 
+<!-- TODO: handle overflows -->
 <div class="flex" in:fly={{ y: 20, duration: 300 }}>
 	<Filtering bind:filter />
-	<section class="col-span-3 flex flex-col items-center w-full" id="main-view">
-		<SearchBar placeholder="Search for party..." />
-		<div class="flex flex-col items-center w-full">
+	<section class="flex flex-col items-center w-full flex-grow" id="main-view">
+		<div class="w-full p-4">
+			<SearchBar placeholder="Search for party..." />
+		</div>
+		<div class="w-full p-4 flex items-center justify-between">
+			<div>Showing 696 entries (0.420 ms)</div>
+			<div>
+				Sort by:
+				<div class="btn-group variant-ghost-surface">
+					<button>Most relevant</button>
+					<button>Most popular</button>
+					<button>Recently added</button>
+				</div>
+			</div>
+		</div>
+		<div class="flex flex-col items-center w-full max-w-screen-lg">
 			{#each parties as party}
-				<figure class="card flex min-w-[50%] items-center justify-between pr-4 bg-primary-200 m-4">
-					<div class="btn-group-vertical">
-						<button class="bg-primary-400">+1</button>
-						<button class="bg-error-400">-1</button>
+				<div class="card flex rounded-none items-center bg-primary-200 m-4 w-full h-32">
+					<div class="btn-group-vertical h-full rounded-r-none ring-outline-token">
+						<button class="flex-grow bg-success-active-token">+1</button>
+						<button class="flex-grow bg-error-active-token">-1</button>
 					</div>
-					<Avatar intials="LD" />
-					<div class="flex flex-col border-r-2 border-gray-300 h-full p-2">
-						<span>{party.name}</span>
-						<span>{party.description}</span>
+					<div class="h-full aspect-square flex items-center justify-center p-5">
+						<Avatar src={faker.image.url()} width="w-full" />
 					</div>
-					<div class="flex flex-col">
-						<span>{party.leader}</span>
-						<span>{party.requirements.commitment}</span>
+					<div class="flex flex-col border-r-2 border-gray-300 h-full p-2 flex-grow">
+						<h2 class="h2">{party.name}</h2>
+						<p>{party.description}</p>
 					</div>
-				</figure>
+
+					<div class="flex flex-col w-1/4 pl-4 space-y-2 h-full p-2 overflow-scroll">
+						<div class="flex items-center w-full space-x-4">
+							<Avatar src={party.leader.avatar} width="w-8" />
+							<span>{party.leader.name}</span>
+						</div>
+						<div class="flex items-center w-full space-x-4">
+							<span class="material-symbols-outlined"> calendar_clock </span>
+							<span>{party.requirements.commitment}</span>
+						</div>
+						<div class="flex w-full space-x-4">
+							<span class="material-symbols-outlined"> tag </span>
+							<div>
+								{#each party.requirements.skills as skill}
+									<span class="chip variant-filled mr-2 mb-2">{skill}</span>
+								{/each}
+							</div>
+						</div>
+					</div>
+				</div>
 			{/each}
 		</div>
 	</section>
