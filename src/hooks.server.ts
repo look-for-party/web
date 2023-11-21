2e08be497859bfc1f328058c242992ed02f24bfe
felixng <ngthinh302@gmail.com>
2023-11-21 03:53:25 -0500

    fix: add `Handle` type to global `handle` hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,8 @@
 // import { handleHooks } from "@lucia-auth/sveltekit"
 import { auth } from '$lib/server/lucia';
-// import { sequence } from "@sveltejs/kit/hooks"
+import type { Handle } from '@sveltejs/kit';
 
-export const handle = async ({ event, resolve }) => {
+export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.auth = auth.handleRequest(event);
 	return await resolve(event);
 };
