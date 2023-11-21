c9e5af8a9a5b9ffcc4e86b493db084ca15ed2212
felixng <ngthinh302@gmail.com>
2023-11-21 04:44:03 -0500

    refactor: separate routes into `(app)` and `(auth)` route groups

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,5 @@
 <script lang="ts">
-	import { AppShell, initializeStores } from '@skeletonlabs/skeleton';
-	import Header from '$src/lib/components/Header/Header.svelte';
 	import '$src/app.postcss';
-
-	// should be in root layout
-	// required only once when implementing Skeleton's Drawer, Modal, and Toast stores
-	// prevent known issues with SvelteKit SSR.
-	// https://www.skeleton.dev/utilities/drawers#prerequisites
-	initializeStores();
 </script>
 
-<AppShell>
-	<svelte:fragment slot="pageHeader">
-		<Header />
-	</svelte:fragment>
-	<!-- Router Slot -->
-	<slot />
-</AppShell>
+<slot />
