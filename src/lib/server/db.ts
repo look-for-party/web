66e0f22cc4b98dca08055c8e0e2126f068278903
felixng <ngthinh302@gmail.com>
2023-11-21 03:59:22 -0500

    feat: add logging for database queries

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -2,7 +2,8 @@ import { drizzle, type PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
 import { DATABASE_URL } from '$env/static/private'; // Pick up dynamic private environment variables, useful for production
+import { dev } from '$app/environment';
 
 // for query purposes
 export const queryClient = postgres(DATABASE_URL); // set to your business demands
-export const db: PostgresJsDatabase = drizzle(queryClient);
+export const db: PostgresJsDatabase = drizzle(queryClient, { logger: dev });
