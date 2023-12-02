1ac458ca6abf588409dda64772522b105fdbadec
felixng <ngthinh302@gmail.com>
2023-12-01 21:41:50 -0500

    feat: banner for `/parties`

---
+++ src/routes/parties/components/Banner.svelte
@@ -0,0 +1,6 @@
+<div class="w-full p-4 flex items-center justify-center h-24 md:h-32 bg-secondary-500">
+	<h1 class="h1 font-bold text-on-secondary-token">
+		<span>Find your party </span>
+		<span>🎉</span>
+	</h1>
+</div>
