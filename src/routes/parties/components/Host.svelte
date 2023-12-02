2e142d7dcdeef99689a8191d8cf4df38c1b4bc24
felixng <ngthinh302@gmail.com>
2023-12-01 21:44:11 -0500

    chore: remove redundant `Host` component

--- src/routes/parties/components/Host.svelte
+++
@@ -1,6 +0,0 @@
-<!-- TODO: party creation view -->
-<form class="form" action="">
-	<label for="host">Host</label>
-	<input type="text" name="host" id="host" />
-	<button type="submit">Submit</button>
-</form>
