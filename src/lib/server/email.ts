970426aad47fd51cf73b41be95f1ad4135dba029
felixng <ngthinh302@gmail.com>
2023-12-29 00:04:54 -0500

    fix: change to Postmark API from SMTP for emails

--- src/lib/server/email.ts
+++ src/lib/server/email.ts
@@ -1,24 +1,53 @@
-import { EMAIL_VERIFY } from '$env/static/private';
+// Kept in case we want to use SMTP instead of Postmark API in the future
+// import { env } from '$env/dynamic/private';
+// import { transporter } from './emailSetup';
+
+// export const sendEmailVerificationLink = async (email: string, token: string) => {
+// 	const url = `http://localhost:${env.PORT}/email-verification/${token}`;
+// 	await transporter.sendMail({
+// 		from: env.EMAIL_VERIFY,
+// 		to: email,
+// 		subject: 'Email Verification',
+// 		text: `Click this link to verify your email: ${url}`,
+// 		html: `<a href="${url}">Click this link to verify your email</a>`
+// 	});
+// };
+
+// export const sendPasswordResetLink = async (email: string, token: string) => {
+// 	const url = `http://localhost:${env.PORT}/password-reset/${token}`;
+// 	await transporter.sendMail({
+// 		from: env.EMAIL_VERIFY,
+// 		to: email,
+// 		subject: 'Password Reset',
+// 		text: `Click this link to reset your password: ${url}`,
+// 		html: `<a href="${url}">Click this link to reset your password</a>`
+// 	});
+// };
+import { env } from '$env/dynamic/private';
 import { transporter } from './emailSetup';
 
 export const sendEmailVerificationLink = async (email: string, token: string) => {
-	const url = `http://localhost:5173/email-verification/${token}`;
-	await transporter.sendMail({
-		from: EMAIL_VERIFY,
-		to: email,
-		subject: 'Email Verification',
-		text: `Click this link to verify your email: ${url}`,
-		html: `<a href="${url}">Click this link to verify your email</a>`
+	const url = `${env.ORIGIN}/email-verification/${token}`;
+	transporter.sendEmail({
+		From: env.EMAIL_VERIFY,
+		To: email,
+		Subject: 'Email Verification',
+		TextBody: `Click this link to verify your email: ${url}`,
+		HtmlBody: `<a href="${url}">Click this link to verify your email</a>`,
+		MessageStream: env.POSTMARK_MESSAGE_STREAM,
+		Tag: 'email-verification'
 	});
 };
 
 export const sendPasswordResetLink = async (email: string, token: string) => {
-	const url = `http://localhost:5173/password-reset/${token}`;
-	await transporter.sendMail({
-		from: EMAIL_VERIFY,
-		to: email,
-		subject: 'Password Reset',
-		text: `Click this link to reset your password: ${url}`,
-		html: `<a href="${url}">Click this link to reset your password</a>`
+	const url = `${env.ORIGIN}/password-reset/${token}`;
+	transporter.sendEmail({
+		From: env.EMAIL_VERIFY,
+		To: email,
+		Subject: 'Password Reset',
+		TextBody: `Click this link to reset your password: ${url}`,
+		HtmlBody: `<a href="${url}">Click this link to reset your password</a>`,
+		MessageStream: env.POSTMARK_MESSAGE_STREAM,
+		Tag: 'password-reset'
 	});
 };
--- src/lib/server/emailSetup.ts
+++ src/lib/server/emailSetup.ts
@@ -1,33 +1,36 @@
-import nodemailer from 'nodemailer';
-import {
-	EMAIL_SERVICE,
-	EMAIL_HOST,
-	EMAIL_PORT,
-	EMAIL_USE_SSL,
-	EMAIL_USER,
-	EMAIL_PASSWORD
-} from '$env/static/private';
+// Kept in case we want to use SMTP instead of Postmark API in the future
+// import nodemailer from 'nodemailer';
+// import { env } from '$env/dynamic/private';
 
-export const transporter = nodemailer.createTransport({
-	pool: true,
-	service: EMAIL_SERVICE,
-	host: EMAIL_HOST,
-	port: Number(EMAIL_PORT),
-	secure: Boolean(EMAIL_USE_SSL),
-	auth: {
-		user: EMAIL_USER,
-		pass: EMAIL_PASSWORD
-	},
-	tls: {
-		// do not fail on invalid certs
-		rejectUnauthorized: false
-	}
-});
+// export const transporter = nodemailer.createTransport({
+// 	pool: true,
+// 	service: env.EMAIL_SERVICE,
+// 	host: env.EMAIL_HOST,
+// 	port: Number(env.EMAIL_PORT),
+// 	secure: Boolean(env.EMAIL_USE_SSL),
+// 	auth: {
+// 		user: env.EMAIL_USER,
+// 		pass: env.EMAIL_PASSWORD
+// 	},
+// 	tls: {
+// 		// do not fail on invalid certs
+// 		rejectUnauthorized: false
+// 	}
+// });
 
-transporter.verify((error) => {
-	if (error) {
-		console.error(error);
-	} else {
-		console.log('🚀 Mailer is ready to take messages');
-	}
-});
+// transporter.verify((error) => {
+// 	if (error) {
+// 		console.error(error);
+// 	} else {
+// 		console.log('🚀 Mailer is ready to take messages');
+// 	}
+// });
+import { building } from '$app/environment';
+import { env } from '$env/dynamic/private';
+import { ServerClient } from 'postmark';
+
+export let transporter: ServerClient;
+
+if (!building) {
+	transporter = new ServerClient(env.POSTMARK_API_KEY);
+}
