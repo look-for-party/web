74c6aa3796155e066466aa783b84fca33a93d386
felixng <ngthinh302@gmail.com>
2023-12-30 19:25:29 -0500

    style: resolve style issues

--- src/routes/(auth)/password-reset/sent/+page.svelte
+++ src/routes/(auth)/password-reset/sent/+page.svelte
@@ -6,7 +6,7 @@
 
 <h1 class="text-xl font-bold md:text-2xl">Check your inbox 😎</h1>
 <p class="text-sm font-light">We've sent you an email with a link to reset your password.</p>
-<div class="w-full btn-group variant-ringed-surface [&>*+*]:border-surface-500">
+<div class="variant-ringed-surface btn-group w-full [&>*+*]:border-surface-500">
 	<a class="flex-1" href="https://mail.google.com/mail/" target="_blank" rel="noopener noreferrer">
 		<span><MdiGmail /></span><span class="flex-1 self-center">Open Gmail</span><span
 			><MdiOpenInNew class="text-xs" /></span
