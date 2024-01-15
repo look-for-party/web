2f7a2cd47f9562bfc008ff38dd6ac6640084ed0c
ixora-0 <phunglegend.2004@gmail.com>
2024-01-15 11:35:27 -0500

    feat: Improve sort selection design.

--- src/routes/(app)/parties/components/PartyList.svelte
+++ src/routes/(app)/parties/components/PartyList.svelte
@@ -1,22 +1,35 @@
 <script lang="ts">
-	import { TabGroup, TabAnchor } from '@skeletonlabs/skeleton';
+	import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
 
 	import type { Party } from '$lib/types';
 	import PartyCard from './PartyCard.svelte';
 
+	import PhSortAscending from '~icons/ph/sort-ascending';
+
 	export let parties: Party[];
+	let sortType: string = 'relevant';
 </script>
 
 <section id="party-list" class="flex flex-1 flex-col gap-8">
 	<div class="flex w-full flex-wrap items-center justify-between gap-2">
 		<p class="text-surface-400-500-token min-w-fit font-light">Showing 696 parties (0.420 ms)</p>
-		<div class="flex items-center space-x-2">
-			<span class="font-semibold">Sort:</span>
-			<TabGroup>
-				<TabAnchor href="#top">Most relevant</TabAnchor>
-				<TabAnchor href="#top">Most popular</TabAnchor>
-				<TabAnchor href="#top">Recently added</TabAnchor>
-			</TabGroup>
+		<div class="flex items-center gap-3">
+			<span class="flex items-center text-lg"> <PhSortAscending />Sort:</span>
+			<RadioGroup
+				background="bg-surface-100-800-token"
+				border="border-none"
+				active="variant-filled-secondary"
+			>
+				<RadioItem bind:group={sortType} name="sortType" value="relevant" class="whitespace-nowrap"
+					>Most relevant</RadioItem
+				>
+				<RadioItem bind:group={sortType} name="sortType" value="popular" class="whitespace-nowrap"
+					>Most popular</RadioItem
+				>
+				<RadioItem bind:group={sortType} name="sortType" value="recent" class="whitespace-nowrap"
+					>Recently added</RadioItem
+				>
+			</RadioGroup>
 		</div>
 	</div>
 	<div class="flex flex-col items-center space-y-8">
